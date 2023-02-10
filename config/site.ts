1dcd2b53a2cdc9e3752f48f83111b8804869353a
Tim <timtb@hey.com>
2023-02-10 07:19:59 -0800

    added og image and edited site config file

--- config/site.ts
+++ config/site.ts
@@ -2,8 +2,8 @@ import { Config } from "@/types"
 
 export const config: Config = {
     title: "Mongolian Sign Language Dictionary",
-    keywords: "Mongolian, Sign, Language, Dictionary",
-    description: "More than 1000 words",
+    keywords: "Mongolian, Sign, Language, Dictionary, sign, app, learn, sign, language, free, mongolian, msl, online, mobile",
+    description: "There has never been an easier way to learn Mongolian Sign Language online. Start learning MSL today",
     url: "https://signapp.net/",
-    og: "https://portfolio-timtbdev.vercel.app/api/og?title=Projects",
+    og: "/og.png",
 }
